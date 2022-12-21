ab74174c7bc7a1544bb2d30cff9f79b0206f9d5f
AlpatyevNikita <chococacaococonut@gmail.com>
2022-12-21 14:52:20 +0300

    added empty project, removed storyboard, LoginViewController is root VC now

---
+++ loginScreen/loginScreen/LoginViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LoginViewController.swift
+//  loginScreen
+//
+//  Created by Nikita Alpatiev on 12/21/22.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .magenta
+    }
+}
+
